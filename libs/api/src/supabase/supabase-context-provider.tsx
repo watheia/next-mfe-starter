93ddc4035a881efc3a022e96094170c85246e07c
Aaron R Miller <amiller@watheia.org>
2022-01-08 17:29:56 -0800

    assign fallbacks in SubabaseProvider

--- libs/api/src/supabase/supabase-context-provider.tsx
+++ libs/api/src/supabase/supabase-context-provider.tsx
@@ -1,17 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
 import React, { ReactNode, useMemo } from 'react';
 import { SupabaseContext } from './supabase-context';
+import env from '../env';
 
 export type SupabaseProviderProps = {
   /**
    * The api gateway
    */
-  publicUrl: string;
+  publicUrl?: string;
 
   /**
    * The api gateway
    */
-  anonKey: string;
+  anonKey?: string;
 
   /**
    * children to be rendered within this theme.
@@ -19,7 +20,11 @@ export type SupabaseProviderProps = {
   children: ReactNode;
 };
 
-export function SupabaseProvider({ publicUrl, anonKey, children }: SupabaseProviderProps) {
+export function SupabaseProvider({
+  publicUrl = env.supabase_url,
+  anonKey = env.public_key,
+  children,
+}: SupabaseProviderProps) {
   const supabase = useMemo(() => createClient(publicUrl, anonKey), [publicUrl, anonKey]);
   return (
     <SupabaseContext.Provider value={{ supabase }}>{children}</SupabaseContext.Provider>
